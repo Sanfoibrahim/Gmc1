7a8809612a26724947e81b9ebe794a5153b7b0a1
Sanfoibrahim <88929555+Sanfoibrahim@users.noreply.github.com>
2021-08-26 12:15:38 +0200

    arkanoide
    cool et simple

---
+++ ball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = vector2.up * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
